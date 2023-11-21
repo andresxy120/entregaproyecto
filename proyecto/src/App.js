4e003842d102c9b261871d8ef2ccef79ea7f2a7a
andresxy120 <achaverrahiguita@gmail.com>
2023-11-21 12:53:38 -0500

    Update App.js

--- proyecto/src/App.js
+++ proyecto/src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Productos from './partpagina/Productos';
+import Carrito from './partpagina/Carrito';
+import Header from './partpagina/Header'
+import Inicio from './partpagina/Inicio';
+import Servicio from './partpagina/Servicio';
+import Registrar from './partpagina/Registro';
+import Noticias from './partpagina/Noticias';
+import Perfil from './partpagina/Perfil';
+import Pagos from './partpagina/Pagos';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="App">
+      <Routes>
+          <Route path='/' element={<Header/>}>
+              <Route path='/' element={<Inicio/>}></Route>
+              <Route path='Productos' element={<Productos/>}></Route>
+              <Route path='Servicio' element={<Servicio/>}></Route>
+              <Route path='Carrito' element={<Carrito/>}></Route>
+              <Route path='Registrar' element={<Registrar/>}></Route>
+              <Route path='Noticias' element={<Noticias/>}></Route>
+              <Route path='Perfil' element={<Perfil/>}></Route>
+              <Route path='Pagos' element={<Pagos/>}></Route>
+              <Route path='*' element={<Inicio/>}></Route>
+          </Route>
+      </Routes>
+    </section>
   );
 }
 
